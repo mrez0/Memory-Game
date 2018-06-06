5e3fc31c68edeb31f0851dedb33659854f23256d
Ahmed Sonbaty <ahmedmohsen1337@hotmail.com>
2018-06-07 02:31:21 +0300

    Implement setGameSettings

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 startGame();
 
 function startGame() {
-    setGameSettings();
-    let cards = createCards();
+    const numberCards = setGameSettings();
+    let cards = createCards( numberCards );
     shuffleCards( cards );
     displayCards( placeholder, cards );
     handleClickEvents();
+}
+
+function setGameSettings() {
+    //Later, we can let the user choose the number of cards to
+    // show in the grid, i.e. difficulty
+    const numberCards = 8;
+    return numberCards;
 }
