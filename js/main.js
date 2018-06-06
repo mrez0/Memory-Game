6aeaa4843242d7172b5ac210de66f027907a48f8
Unknown <ahmedmohsen1337@hotmail.com>
2018-06-07 02:54:01 +0300

    Implement createCards function

--- js/main.js
+++ js/main.js
@@ -14,3 +14,16 @@ function setGameSettings() {
     const numberCards = 8;
     return numberCards;
 }
+
+function createCards(numberCards) {
+    let cards = [];
+
+    for( let x = 0; x < numberCards; x++ ) {
+        let card = document.createElement('div');
+        card.textContent = x + 1; //Numbers on cards start with 1 not 0
+        cards.push(card);
+        cards.push(card);
+    }
+
+    return cards;
+}
