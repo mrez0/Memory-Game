06dbd7699074735c73a4a8dff7b35eb80d95767d
Unknown <ahmedmohsen1337@hotmail.com>
2018-06-07 03:08:49 +0300

    Reformatting

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ startGame();
 
 function startGame() {
     const setting = setGameSettings();
-    let cards = createCards( setting.numberCards );
-    cards = shuffle( cards );
-    displayCards( setting.canvasClass, cards );
+    let cards = createCards(setting.numberCards);
+    cards = shuffle(cards);
+    displayCards(setting.canvasClass, cards);
     handleClickEvents();
 }
 
@@ -20,7 +20,7 @@ function setGameSettings() {
 function createCards(numberCards) {
     let cards = [];
 
-    for( let x = 0; x < numberCards; x++ ) {
+    for (let x = 0; x < numberCards; x++) {
         let card = document.createElement('div');
         card.textContent = x + 1; //Numbers on cards start with 1 not 0
         cards.push(card);
@@ -32,20 +32,21 @@ function createCards(numberCards) {
 
 //Shuffle array
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     // While there remain elements to shuffle...
     while (0 !== currentIndex) {
 
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
 
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
     }
 
     return array;
-  }
+}
